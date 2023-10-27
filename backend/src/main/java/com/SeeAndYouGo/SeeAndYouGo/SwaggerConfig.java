35000d4d9e9ff73e3e0ef8cec9ec91c2344aabe8
shxnzxxn <122579839+shxnzxxn@users.noreply.github.com>
2023-10-27 18:25:23 +0900

    feat: Swagger (#25)
    * feat: Swagger
    
    * feat: Swagger

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/SwaggerConfig.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/SwaggerConfig.java
@@ -1,2 +1,32 @@
-package com.SeeAndYouGo.SeeAndYouGo;public class SwaggerConfig {
+package com.SeeAndYouGo.SeeAndYouGo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SeeAndYouGO Swagger")
+                .description("SeeAndYouGo")
+                .version("2.0")
+                .build();
+    }
 }
