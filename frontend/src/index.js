59e6b5ff1c5c597a5c1ec7d866831a20328436b0
VertexToEdge <vertex@g.cnu.ac.kr>
2023-11-21 23:57:34 +0900

    refactor(frontend): replace npm module react-ga to react-ga4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 const TRACKING_ID = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID);
+ReactGA.initialize(TRACKING_ID)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
