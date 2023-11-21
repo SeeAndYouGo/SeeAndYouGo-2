19e70775ba0b6e1ad7a5a58e28bd0aa349d4fd43
VertexToEdge <vertex@g.cnu.ac.kr>
2023-11-22 01:19:08 +0900

    fix(frontend): add prefix "REACT_APP_" in google analytics env

--- frontend/src/RouteChangeTracker.js
+++ frontend/src/RouteChangeTracker.js
@@ -8,7 +8,7 @@ const RouteChangeTracker = () => {
   // localhost는 기록하지 않음
   useEffect(() => {
     if (!window.location.href.includes("localhost")) {
-      ReactGA.initialize(process.env.GOOGLE_ANALYTICS_TRACKING_ID);
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
       setInitialized(true);
     }
   }, []);
@@ -23,7 +23,7 @@ const RouteChangeTracker = () => {
 
   // 개발용
   useEffect(() => {
-    ReactGA.initialize(process.env.GOOGLE_ANALYTICS_TRACKING_ID);
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
     ReactGA.set({ page: location.pathname });
     ReactGA.send("pageview");
   }, [location]);
