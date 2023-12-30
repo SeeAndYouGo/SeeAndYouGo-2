96cc79b498377827a4ef687e97d975bbf6ebf61a
rlcz1 <rbf0311@gmail.com>
2023-12-31 04:34:25 +0900

    feat: Hamburger Button

--- frontend/src/components/MainPage/Header.js
+++ frontend/src/components/MainPage/Header.js
@@ -1,19 +1,49 @@
 import styled from "@emotion/styled";
+import React, { useState } from "react";
+import SideBar from "../SideBar";
+import { Link } from "react-router-dom";
 
 const HeaderContainer = styled.div`
-	width: 100%;
-	padding-top: 0;
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  max-width: 360px;
+	margin: 0 auto;
+  padding-top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+	line-height: 50px;
 `;
 
 const Header = () => {
-	return (
-		<HeaderContainer>
-			<h1 style={{ margin: 0, fontFamily: "Jua" }}>SeeAndYouGo</h1>
-		</HeaderContainer>
-	);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(true);
+  };
+  return (
+    <>
+      <HeaderContainer>
+        <h1 style={{ margin: 0, fontSize: 28 }}>
+					<Link to="/" style={{fontFamily: "Jua", color: "#fff", fontWeight: 500}}>
+          	SeeAndYouGo
+					</Link>
+        </h1>
+        <span
+          onClick={toggleMenu}
+          className="material-symbols-outlined"
+          style={{
+            position: "absolute",
+            right: 15,
+            fontSize: 30,
+            cursor: "pointer",
+						color: "#fff"
+          }}
+        >
+          menu
+        </span>
+      </HeaderContainer>
+      <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
+  );
 };
 
 export default Header;
