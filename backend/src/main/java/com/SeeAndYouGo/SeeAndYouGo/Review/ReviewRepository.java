b1e6d7d03a9cc5ee8c55e014bf66ee9fbfb0e4f4
yyeennyy <69946286+yyeennyy@users.noreply.github.com>
2024-02-16 11:02:00 +0900

    fix: top review count limit 3 (#134)
    * fix: top-review limit 3
    
    * fix: top-review limit 3
    
    * fix: top-review limit 3

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Review/ReviewRepository.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Review/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.SeeAndYouGo.SeeAndYouGo.Review;
 
+import com.SeeAndYouGo.SeeAndYouGo.Menu.Dept;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,15 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findTop3ByRestaurantNameAndMadeTimeStartingWithOrderByMadeTimeDesc(@Param("restaurantName") String restaurantName, @Param("date") String date);
+
+    @Query("SELECT r FROM Review r " +
+            "WHERE r.restaurant.name = :restaurantName " +
+            "AND r.menu.dept = :dept " +
+            "AND FUNCTION('SUBSTRING', r.madeTime, 1, 10) = FUNCTION('SUBSTRING', :date, 1, 10) " +
+            "ORDER BY r.madeTime DESC")
+    List<Review> findTop3ByRestaurantDeptOrderByMadeTimeDesc(@Param("restaurantName") String restaurantName,
+                                                             @Param("date") String date,
+                                                             @Param("dept") Dept dept);
 
     @Query("select r from Review r " +
             "where FUNCTION('SUBSTRING', r.madeTime, 1, 10) = FUNCTION('SUBSTRING', :date, 1, 10) " +
