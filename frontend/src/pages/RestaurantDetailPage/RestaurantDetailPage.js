1135725f92884254f56034ba3ed468e358c1fbc2
SeoJeongchan <wjdcks822@gmail.com>
2024-03-18 15:07:17 +0900

    feat: detail page responsive (#203)

--- frontend/src/pages/RestaurantDetailPage/RestaurantDetailPage.js
+++ frontend/src/pages/RestaurantDetailPage/RestaurantDetailPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "@emotion/styled";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "../../App.css";
@@ -7,6 +8,33 @@ import MenuInfo from "./MenuInfo";
 import ReviewWrite from "./ReviewWrite";
 import TopReview from "./TopReview";
 
+const DetailPageWrapper = styled.div`
+	@media (min-width: 616px) {
+		padding: 20px;
+	}
+`;
+
+const DetailContent = styled.div`
+	@media (min-width: 576px) {
+		display: flex;
+	};
+`;
+
+const Content1 = styled.div`
+	@media (min-width: 576px) {
+		width: 50%;
+		min-width: 320px;
+	}
+`;
+
+const Content2 = styled.div`
+	@media (min-width: 576px) {
+		width: 40%;
+		margin-left: auto;
+		margin-top: 55px;
+	}
+`;
+
 const RestaurantDetailPage = () => {
 	const params = useParams();
 	const restaurant = parseInt(params.restaurant);
@@ -14,15 +42,21 @@ const RestaurantDetailPage = () => {
 
 	return (
 		<>
-			<div className="App2">
+			<DetailPageWrapper className="App2">
 				<DetailHeader idx={restaurant} />
-				<MenuInfo idx={restaurant} />
-				<ReviewWrite restaurantNum={restaurant} deptNum={nowDept} />
-				<TopReview
-					idx={restaurant}
-					nowDept={nowDept === 1 ? "STUDENT" : "STAFF"}
-				/>
-			</div>
+				<DetailContent>
+					<Content1>
+						<MenuInfo idx={restaurant} />
+						<ReviewWrite restaurantNum={restaurant} deptNum={nowDept} />
+					</Content1>
+					<Content2>
+						<TopReview
+							idx={restaurant}
+							nowDept={nowDept === 1 ? "STUDENT" : "STAFF"}
+						/>
+					</Content2>
+				</DetailContent>
+			</DetailPageWrapper>
 			<div className="blankSpace" style={{ marginBottom: 20 }}>&nbsp;</div>
 		</>
 	);
