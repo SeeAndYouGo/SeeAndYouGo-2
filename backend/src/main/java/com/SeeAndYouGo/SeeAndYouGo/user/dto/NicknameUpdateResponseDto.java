8f772fe387a7823944ab9f353ce1d0b3788c08b2
skj4244 <skj4244@naver.com>
2024-03-20 00:29:34 +0900

    feat: auto change camel case to snake

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/user/dto/NicknameUpdateResponseDto.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/user/dto/NicknameUpdateResponseDto.java
@@ -1,9 +1,12 @@
 package com.SeeAndYouGo.SeeAndYouGo.user.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class NicknameUpdateResponseDto {
     private final String error;
     private final String message;
