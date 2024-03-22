a42c648558e04d57cec0a6e387619f3b9bb31eb7
yyeennyy <69946286+yyeennyy@users.noreply.github.com>
2024-03-22 16:58:36 +0900

    refactor: minimizes review post request dto (#209)

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Review/dto/ReviewRequestDto.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Review/dto/ReviewRequestDto.java
@@ -1,28 +1,11 @@
 package com.SeeAndYouGo.SeeAndYouGo.Review.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Getter @Setter // setter를 열어줘야 formdata가 바인딩될 수 있다.
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReviewRequestDto {
-    @Builder
-    private ReviewRequestDto(MultipartFile image, boolean anonymous, String restaurant, String dept, String menuName, Double rate, String writer, String nickName, String comment, String imgUrl) {
-        this.image = image;
-        this.anonymous = anonymous;
-        this.restaurant = restaurant;
-        this.dept = dept;
-        this.menuName = menuName;
-        this.rate = rate;
-        this.writer = writer;
-        this.nickName = nickName;
-        this.comment = comment;
-        this.imgUrl = imgUrl;
-    }
-
     private String restaurant;
     private String dept;
     private String menuName;
@@ -30,8 +13,5 @@ public class ReviewRequestDto {
     private String writer;
     private String nickName;
     private String comment;
-    private String imgUrl;
     private boolean anonymous;
-    private MultipartFile image;
-
 }
