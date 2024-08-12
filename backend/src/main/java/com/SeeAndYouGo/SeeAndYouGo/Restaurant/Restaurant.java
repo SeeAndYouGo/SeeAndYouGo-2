60d927b184c1741923cf88f9e99eca646358ea21
shxnzxxn <skj4244@naver.com>
2024-08-12 16:20:15 +0900

    remove: Restaurant Entity

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Restaurant/Restaurant.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Restaurant/Restaurant.java
@@ -1,73 +1,23 @@
 package com.SeeAndYouGo.SeeAndYouGo.Restaurant;
 
-import com.SeeAndYouGo.SeeAndYouGo.Connection.Connection;
-import com.SeeAndYouGo.SeeAndYouGo.Menu.Menu;
-import com.SeeAndYouGo.SeeAndYouGo.Review.Review;
-import lombok.Builder;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
 @Getter
-@Setter
-@NoArgsConstructor
-public class Restaurant {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "restaurant_id")
-    private Long Id;
-    private String name;
-    private Integer capacity;
-    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
-    private List<Menu> menuList = new ArrayList<>();
-    private String date;
-    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
-    private List<Connection> connectionList = new ArrayList<>();
+public enum Restaurant {
+    제1학생회관(99),
+    제2학생회관(99),
+    제3학생회관(99),
+    상록회관(99),
+    생활과학대(99);
 
-    private Double restaurantRate = 0.0;
-    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
-    private List<Review> reviewList = new ArrayList<>();
+    private final Integer capacity;
 
-    @Builder
-    public Restaurant(String name, String date) {
-        this.setName(name);
-        this.setDate(date);
-        setCapacity(name);
+    Restaurant(Integer capacity) {
+        this.capacity = capacity;
     }
 
-    private void setCapacity(String name){
-        if(name.contains("1학")) capacity = 486;
-        else if(name.contains("2학")) capacity = 392;
-        else if(name.contains("3학")) capacity = 273;
-        else if(name.contains("상록")) capacity = 140;
-        else if(name.contains("생활")) capacity = 190;
-    }
-
-    @Override
-    public String toString(){
-        return name+" "+date + " " + menuList;
-    }
-
-    public void updateTotalRate() {
-        double sum = 0.0;
-        int count = 0;
-
-        for (Menu menu : this.menuList) {
-            if(menu.getRate() != 0.0) {
-                // 리뷰가 작성된 식당의 평균들을 계산해야함.
-                count++;
-                sum += menu.getRate();
-            }
-        }
-
-        this.restaurantRate = count == 0 ? 0 : sum/count;
-    }
-
-    public static String parseName(String name) {
+    public static String parseName(String name){
         if (name.contains("1")) return "1학생회관";
         else if (name.contains("2")) return "2학생회관";
         else if (name.contains("3")) return "3학생회관";
