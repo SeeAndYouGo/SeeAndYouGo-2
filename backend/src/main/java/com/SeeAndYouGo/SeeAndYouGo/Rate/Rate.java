9f366e43bd53a951f278cacfd9c6cdf4e6b7d3d6
shxnzxxn <skj4244@naver.com>
2024-08-13 16:16:35 +0900

    refactor: restaurant 엔티티 변경으로 인한 수정

---
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Rate/Rate.java
@@ -0,0 +1,49 @@
+package com.SeeAndYouGo.SeeAndYouGo.Rate;
+
+import com.SeeAndYouGo.SeeAndYouGo.Restaurant.Restaurant;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Rate {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rate_id")
+    private Long id;
+
+    @Enumerated(value = EnumType.STRING)
+    private Restaurant restaurant;
+
+    @ColumnDefault("0.0")
+    private double rate;
+
+    @ColumnDefault("0")
+    private Integer reflectedNumber;
+
+    @Builder
+    public Rate(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public void reflectRate(double rate){
+        double totalSum = rate * reflectedNumber;
+        double updateSum = totalSum + rate;
+
+        this.reflectedNumber++;
+        this.rate = updateSum/reflectedNumber;
+    }
+
+    public void exceptRate(double rate) {
+        double totalSum = rate * reflectedNumber;
+        double updateSum = totalSum - rate;
+
+        this.reflectedNumber--;
+        this.rate = updateSum / reflectedNumber;
+    }
+}
