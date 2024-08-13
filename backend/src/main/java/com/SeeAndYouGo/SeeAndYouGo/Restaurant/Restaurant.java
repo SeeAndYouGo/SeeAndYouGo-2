bfe75fec298480619e5256011251c0578e29aaa9
shxnzxxn <skj4244@naver.com>
2024-08-13 14:57:16 +0900

    refactor: restaurant 엔티티 변경으로 인한 수정

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Restaurant/Restaurant.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Restaurant/Restaurant.java
@@ -3,24 +3,34 @@ package com.SeeAndYouGo.SeeAndYouGo.Restaurant;
 
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 @Getter
 public enum Restaurant {
-    제1학생회관(99),
-    제2학생회관(99),
-    제3학생회관(99),
-    상록회관(99),
-    생활과학대(99);
+    제1학생회관(486, Location.of(36.367838, 127.343160)),
+    제2학생회관(392, Location.of(36.365959, 127.345828)),
+    제3학생회관(273, Location.of(36.371479, 127.344841)),
+    상록회관(140, Location.of(36.368605, 127.350374)),
+    생활과학대(190, Location.of(36.376309, 127.343158));
 
     private final Integer capacity;
+    private final Location location;
 
-    Restaurant(Integer capacity) {
+    Restaurant(int capacity, Location location) {
         this.capacity = capacity;
+        this.location = location;
     }
 
+    Restaurant(int capacity, double longitude, double latitude) {
+        this.capacity = capacity;
+        this.location = Location.of(longitude, latitude);
+    }
+
+
     public static String parseName(String name){
-        if (name.contains("1")) return "1학생회관";
-        else if (name.contains("2")) return "2학생회관";
-        else if (name.contains("3")) return "3학생회관";
+        if (name.contains("1")) return "제1학생회관";
+        else if (name.contains("2")) return "제2학생회관";
+        else if (name.contains("3")) return "제3학생회관";
         else if (name.contains("4") || name.contains("상록회관")) return "상록회관";
         else if (name.contains("5") || name.contains("생활과학대") ) return "생활과학대";
         return name;
