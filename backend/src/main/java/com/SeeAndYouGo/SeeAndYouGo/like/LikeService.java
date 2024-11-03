9b6b5174f1ea08d7ad0d1f06b0c33cd271ec7855
yyeennyy <kye3314@gmail.com>
2024-11-03 11:13:25 +0900

    feat: like caching

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/like/LikeService.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/like/LikeService.java
@@ -7,8 +7,13 @@ import com.SeeAndYouGo.SeeAndYouGo.like.dto.LikeResponseDto;
 import com.SeeAndYouGo.SeeAndYouGo.user.User;
 import com.SeeAndYouGo.SeeAndYouGo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -18,14 +23,16 @@ public class LikeService {
     private final TokenProvider tokenProvider;
     private final LikeRepository likeRepository;
     private final UserRepository userRepository;
+    private final RedisTemplate redisTemplate;
 
     @Transactional
+
     public LikeResponseDto postLikeCount(Long reviewId, String tokenId) {
         Review review = reviewRepository.findById(reviewId).get();
         String userEmail = tokenProvider.decodeToEmail(tokenId);
         User user = userRepository.findByEmail(userEmail);
 
-        if(review.getWriterEmail().equals(userEmail)){
+        if (review.getWriterEmail().equals(userEmail)) {
             return LikeResponseDto.builder()
                     .mine(true)
                     .like(false)
@@ -34,9 +41,9 @@ public class LikeService {
 
         boolean isLike = likeRepository.countByReviewAndUser(review, user) > 0;
 
-        if(isLike){
+        if (isLike) {
             return deleteLike(review, user);
-        }else{
+        } else {
             return postLike(review, user);
         }
 
@@ -68,13 +75,57 @@ public class LikeService {
     }
 
     public boolean isLike(Review review, String userEmail) {
-        if(userEmail.equals("")) return false;
+        if (userEmail.equals("")) return false;
 
         User user = userRepository.findByEmail(userEmail);
-        if(likeRepository.existsByReviewAndUser(review, user)){
+        if (likeRepository.existsByReviewAndUser(review, user)) {
             return true;
         }
 
         return false;
     }
+
+    public String getWriter(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId).get();
+        return review.getWriterEmail();
+    }
+
+//    @Scheduled(fixedRate = 60000 * 60) // 1시간마다
+//    public void backupLikeCachingData() {
+//        HashOperations redisHash = redisTemplate.opsForHash();
+//        Set<String> keys = redisTemplate.keys("review:like:*");
+//
+//        Map<User, Review> likes = new HashMap<>();
+//        Map<User, Review> unlikes = new HashMap<>();
+//
+//        if (keys != null) {
+//            for (String key : keys) {
+//                Map<String, Integer> userLikes = redisHash.entries(key); // 각 키의 모든 해시 가져오기
+//
+//                for (Map.Entry<String, String> entry : userLikes.entrySet()) {
+//                    String userEmail = entry.getKey();
+//                    Integer value = entry.getValue();
+//
+//                    User user = userRepository.findByEmail(userEmail);
+//
+//
+//                    if (value.equals(1)) {
+//                        // User와 Review 객체를 생성하고 likes에 추가
+//                        likes.put(user, new Review(key.replace("review:like:", "")));
+//                    } else if (value.equals(0)) {
+//                        // User와 Review 객체를 생성하고 unlikes에 추가
+//                        unlikes.put(user, new Review(key.replace("review:like:", "")));
+//                    }
+//                }
+//            }
+//        }
+//
+//        // 캐시 데이터 List<Like>
+//
+//
+//        HashOperations ops = redisHash;
+//        ops.get
+//
+//        likeRepository.saveAll(backupTargets);
+//    }
 }
