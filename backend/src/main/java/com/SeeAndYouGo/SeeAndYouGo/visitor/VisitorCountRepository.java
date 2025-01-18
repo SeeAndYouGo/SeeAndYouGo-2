8b1e5706a495496fb553a33f816ab9a1fd161cd3
Yeeun Kim <69946286+yyeennyy@users.noreply.github.com>
2025-01-18 22:07:48 +0900

    fix: add missing restore logic for visitor count (#324)

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/visitor/VisitorCountRepository.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/visitor/VisitorCountRepository.java
@@ -1,20 +1,19 @@
 package com.SeeAndYouGo.SeeAndYouGo.visitor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
 public interface VisitorCountRepository extends JpaRepository<VisitorCount, Long> {
 
-    @Query("SELECT v FROM VisitorCount v WHERE v.isTotal = false AND DATE(v.createdAt) = CURRENT_DATE")
-    Optional<VisitorCount> findTodayTempData();
-
     void deleteByIsTotalFalse();
 
-    Optional<VisitorCount> findByIsTotalTrue();
+    @Query("SELECT v FROM VisitorCount v WHERE v.isTotal = false AND DATE(v.createdAt) = CURRENT_DATE")
+    Optional<VisitorCount> findRecentTodayBackup();
+
+    @Query("SELECT v FROM VisitorCount v WHERE v.isTotal = true AND DATE(v.createdAt) = CURRENT_DATE")
+    Optional<VisitorCount> findRecentTotalBackup();
 }
