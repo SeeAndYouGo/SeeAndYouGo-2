0ceb7379f8026385e2bb7c71dfa25ec6288bf843
SeoJeongchan <wjdcks822@gmail.com>
2025-05-10 22:44:16 +0900

    refactor: check report count

--- frontend/src/components/Review/ReviewReport.js
+++ frontend/src/components/Review/ReviewReport.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 import { useDispatch } from "react-redux";
 import { showToast } from "../../redux/slice/ToastSlice";
-import { putWithToken } from "../../api";
+import { putWithToken, erase } from "../../api";
 
 const ReportButton = styled.span`
 	width: 25px;
@@ -20,12 +20,31 @@ const ReviewReport = ({ reportTarget }) => {
 
 		putWithToken(url)
 			.then((res) => {
-				if (res.data.success === true) {
+				if (res.data.reportCount >= 10) {
+					erase(`/review/report/${reportTarget}`)
+						.then((res) => {
+							if (res.data.success === true) {
+								dispatch(showToast({ contents: "review", toastIndex: 10 }));
+							} else {
+								throw new Error("Failed to delete review after 10 reports");
+							}
+						})
+						.catch((error) => {
+							dispatch(showToast({ contents: "review", toastIndex: 8 }));
+							console.log(error);
+						});
+					setTimeout(() => {
+						window.location.reload();
+					}, 1000);
+				} else if (res.data.reportCount < 10 && res.data.reportCount > 0) {
 					dispatch(showToast({ contents: "review", toastIndex: 5 }));
+				} else {
+					throw new Error("Invalid report count");
 				}
 			})
-			.catch(() => {
+			.catch((error) => {
 				dispatch(showToast({ contents: "review", toastIndex: 6 }));
+				console.log(error);
 			});
 	};
 
