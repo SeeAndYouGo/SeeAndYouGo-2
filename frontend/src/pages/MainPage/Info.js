859d34c3ee0191949585c044edc6398ec2de9b3a
Gyuri <rbf0311@gmail.com>
2025-06-29 03:57:07 +0900

    fix: reset weekly menu modal scroll and hide weekend menus

--- frontend/src/pages/MainPage/Info.js
+++ frontend/src/pages/MainPage/Info.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styled from "@emotion/styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapLocationDot, faCalendarDays } from "@fortawesome/free-solid-svg-icons";
@@ -98,6 +98,14 @@ const operatingTime = {
 
 const Info = ({ idx = 1 }) => {
   const [visible, setVisible] = useState({ map: false, menu: false });
+  const menuModalRef = useRef(null);
+
+  const handleMenuClose = () => {
+    if (menuModalRef.current) {
+      menuModalRef.current.resetScroll();
+    }
+    setVisible((prev) => ({ ...prev, menu: false }));
+  };
 
 	return (
 		<Container>
@@ -116,19 +124,19 @@ const Info = ({ idx = 1 }) => {
 			<div>
 				<div style={{display: 'flex', marginLeft: 'auto'}}>
         {idx !== 1 ? (
-						<>
-							<ModalContent
-								style={{ marginRight: 5 }}
-								onClick={() => setVisible({...visible, menu: true})}
-							>
-								<FontAwesomeIcon icon={faCalendarDays} />
-								<p style={{ fontSize: 11 }}>식단표</p>
-							</ModalContent>
-              <Modal visible={visible.menu} onClose={() => setVisible({...visible, menu: false})}>
-								<MenuTableModal idx={idx} onClose={() => setVisible({...visible, menu: false})} />
-							</Modal>
-						</>
-					) : null}
+					<>
+						<ModalContent
+							style={{ marginRight: 5 }}
+							onClick={() => setVisible({...visible, menu: true})}
+						>
+							<FontAwesomeIcon icon={faCalendarDays} />
+							<p style={{ fontSize: 11 }}>식단표</p>
+						</ModalContent>
+            <Modal visible={visible.menu} onClose={handleMenuClose}>
+							<MenuTableModal ref={menuModalRef} idx={idx} />
+						</Modal>
+					</>
+				) : null}
 					<ModalContent
             onClick={() => setVisible({...visible, map: true})}
 					>
