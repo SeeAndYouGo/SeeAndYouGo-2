c31c37794841e1b0bcc932d6427938a534895e2b
SeoJeongchan <wjdcks822@gmail.com>
2025-07-13 01:55:19 +0900

    381 feat edit and delete dish name (#387)
    * feat: edit and delete dish name
    
    * fix: menu separation in admin main menu page

--- frontend/src/pages/AdminPage/SetMainMenuPage.js
+++ frontend/src/pages/AdminPage/SetMainMenuPage.js
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { showToast } from "../../redux/slice/ToastSlice";
 import { get, put } from "../../api/index";
 
-const SubmitButton = styled.button`
+const Button = styled.button`
 	background: white;
 	border: 2px solid #ddd;
 	border-radius: 10px;
@@ -20,6 +20,7 @@ const SetMainMenuPage = () => {
 	const [isAdmin, setIsAdmin] = useState(false);
 	const [password, setPassword] = useState("");
 	const [buttonDisabled, setButtonDisabled] = useState(false);
+	const [showTotal, setShowTotal] = useState(false);
 	const dispatch = useDispatch();
 
 	const handlePasswordChange = (e) => {
@@ -47,11 +48,11 @@ const SetMainMenuPage = () => {
 		const fetchData = async () => {
 			const response = await get("/weekly-menu");
 			const result = response.data;
-			console.log(result, "가져온 데이터 확인");
 			return result;
 		};
 		fetchData().then((data) => {
-			setMenuList(data);
+			// data 중 restaurantName이 제1학생회관인 경우 제외
+			setMenuList(data.filter((menu) => menu.restaurantName !== "제1학생회관"));
 		});
 	}, []);
 
@@ -80,6 +81,26 @@ const SetMainMenuPage = () => {
 			});
 		});
 	};
+
+	const handleShowTotal = (e) => {
+		const nowValue = Number(e.target.value); // 선택한 radio 버튼의 value 값
+		const changeValue = nowValue === 2; // 바뀐 상태
+		setShowTotal(changeValue);
+		
+		if (changeValue) {
+			// 전체보기 버튼 클릭시, 전체 데이터 재요청
+			const fetchData = async () => {
+				const response = await get("/weekly-menu");
+				const result = response.data;
+				return result;
+			};
+			fetchData().then((data) => {
+				setMenuList(data);
+			});
+		} else { // 1학 제외 버튼 클릭시, menulist의 각 menu의 restaurantName이 제1학생회관인 경우 제외
+			setMenuList((prev) => prev.filter((menu) => menu.restaurantName !== "제1학생회관"));
+		}
+	}
 
 	const handleSubmit = async () => {
 		if (buttonDisabled) return;
@@ -112,15 +133,37 @@ const SetMainMenuPage = () => {
 								onKeyDown={handleKeyPress}
 							/>
 						</label>
-						<SubmitButton onClick={handleAdminLogin}>로그인</SubmitButton>
+						<Button onClick={handleAdminLogin}>로그인</Button>
 					</div>
-				) : (
+				) :
+				(
 					<div className="AdminPage">
 						<div style={{ textAlign: "center", margin: "60px 0 20px" }}>
 							<p>비밀 주소입니다. 어떻게 오셨죠?</p>
 							<p>메인 메뉴가 선정되어 있는 부분은 배경색이 표시!!</p>
 							<p>한 번 전송하면 선택했던 데이터는 초기화</p>
 							<p>But, 화면에는 선택 표기가 남아있습니다..</p>
+						</div>
+
+						<div style={{ textAlign: "center", marginBottom: "20px" }}>
+							<label style={{ marginRight: "10px" }}>
+								<input
+										type="radio"
+										value={1}
+										onChange={handleShowTotal}
+										checked={!showTotal}
+										/>
+											1학 제외
+							</label>
+							<label>
+									<input
+										type="radio"
+										value={2}
+										onChange={handleShowTotal}
+										checked={showTotal}
+										/>
+											전체보기
+							</label>
 						</div>
 
 						{menuList.map((val1, idx1) => {
@@ -161,7 +204,7 @@ const SetMainMenuPage = () => {
 								</div>
 							);
 						})}
-						<SubmitButton
+						<Button
 							type="confirm"
 							disabled={buttonDisabled}
 							onClick={() => {
@@ -171,7 +214,7 @@ const SetMainMenuPage = () => {
 							}}
 						>
 							전송
-						</SubmitButton>
+						</Button>
 						<div style={{ height: "100px" }}></div>
 					</div>
 				)
