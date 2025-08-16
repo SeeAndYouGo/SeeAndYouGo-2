0baef3bb45b8b949a35245631f3c59733450fc1f
yyeennyy <kye3314@gmail.com>
2025-08-16 17:12:18 +0900

    feat: newDishList base

---
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/menu/cache/ClearMainDishCache.java
@@ -0,0 +1,26 @@
+package com.SeeAndYouGo.SeeAndYouGo.menu.cache;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 메인 메뉴 캐시를 무효화(삭제)하는 어노테이션
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClearMainDishCache {
+    
+    /**
+     * Restaurant 파라미터의 이름을 지정
+     * 기본값은 "restaurant"
+     */
+    String restaurantParam() default "restaurant";
+    
+    /**
+     * 모든 레스토랑의 캐시를 삭제할지 여부
+     * true인 경우 restaurantParam은 무시되고 다 삭제됨
+     */
+    boolean clearAll() default false;
+}
